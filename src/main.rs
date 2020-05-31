435fbaea73bbb1af7f7c8b30cb72b74e9fe67cb9
jeffladiray <ladirayjeff@gmail.com>
2020-05-31 15:04:18 +0200

    feat: add fov

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use tcod::colors::*;
 use tcod::console::*;
 use tcod::input::Key;
 use tcod::input::KeyCode::*;
+use tcod::map::{ FovAlgorithm, Map as FovMap };
 use std::cmp;
 use rand::Rng;
 
@@ -15,20 +16,44 @@ const LIMIT_FPS: i32 = 24;
 const MAP_WIDTH: i32 = 80;
 const MAP_HEIGHT: i32 = SCREEN_HEIGHT - 5;
 
-const COLOR_DARK_WALL: Color = Color { r: 0, g: 0, b: 100 };
+const COLOR_DARK_WALL: Color = Color { 
+    r: 0,
+    g: 0,
+    b: 100
+};
+
 const COLOR_DARK_GROUND: Color = Color {
     r: 50,
     g: 50,
     b: 150,
 };
 
+const COLOR_LIGHT_WALL: Color = Color {
+    r: 130,
+    g: 110,
+    b: 50,
+};
+
+const COLOR_LIGHT_GROUND: Color = Color {
+    r: 200,
+    g: 180,
+    b: 50,
+};
+
 const ROOM_MAX_SIZE: i32 = 10;
 const ROOM_MIN_SIZE: i32 = 5;
 const MAX_ROOMS: i32 = 10;
 
+// NOTICE: FOV parameters
+const FOV_ALGORITHM: FovAlgorithm = FovAlgorithm::Basic;
+const FOV_LIGHT_WALLS: bool = true;
+const TORCH_RADIUS: i32 = 10;
+
+
 struct Tcod {
     root: Root,
     con: Offscreen,
+    fov: FovMap,
 }
 
 #[derive(Debug)]
@@ -175,21 +200,30 @@ fn make_map(player: &mut GameObject) -> Map {
     map
 }
 
-fn render_all(tcod: &mut Tcod, game: &Game, game_objects: &[GameObject]) {
-    for game_object in game_objects {
-        game_object.draw(&mut tcod.con);
+fn render_all(tcod: &mut Tcod, game: &Game, game_objects: &[GameObject], fov_need_recompute: bool) {
+    if fov_need_recompute {
+        let player = &game_objects[0];
+        tcod.fov.compute_fov(player.x, player.y, TORCH_RADIUS, FOV_LIGHT_WALLS, FOV_ALGORITHM);
     }
 
     for y in 0..MAP_HEIGHT {
         for x in 0..MAP_WIDTH {
+            let visible = tcod.fov.is_in_fov(x, y);
             let is_wall = game.map[x as usize][y as usize].block_sight;
-            if is_wall {
-                tcod.con
-                    .set_char_background(x, y, COLOR_DARK_WALL, BackgroundFlag::Set);
-            } else {
-                tcod.con
-                    .set_char_background(x, y, COLOR_DARK_GROUND, BackgroundFlag::Set);
-            }
+            let color = match (visible, is_wall) {
+                (false, true) => COLOR_DARK_WALL,
+                (false, false) => COLOR_DARK_GROUND,
+                (true, false) => COLOR_LIGHT_GROUND,
+                (true, true) => COLOR_LIGHT_WALL,
+            };
+            tcod.con
+                .set_char_background(x, y, color, BackgroundFlag::Set);
+        }
+    }
+
+    for game_object in game_objects {
+        if tcod.fov.is_in_fov(game_object.x, game_object.y) {
+            game_object.draw(&mut tcod.con);
         }
     }
 
@@ -237,25 +271,40 @@ fn main() {
         .title("Rust-rogue")
         .init();
 
-    let con = Offscreen::new(MAP_WIDTH, MAP_HEIGHT);
-
-    let mut tcod = Tcod { root, con };
+    let mut tcod = Tcod {
+        root,
+        con: Offscreen::new(MAP_WIDTH, MAP_HEIGHT),
+        fov: FovMap::new(MAP_WIDTH, MAP_HEIGHT),
+    };
 
     let player = GameObject::new(25, 23, '@', WHITE);
     let mut game_objects = [player];
 
     let game = Game { map: make_map(&mut game_objects[0]) };
 
+    for y in 0..MAP_HEIGHT {
+        for x in 0..MAP_WIDTH {
+            tcod.fov.set(
+                x,
+                y,
+                !game.map[x as usize][y as usize].block_sight,
+                !game.map[x as usize][y as usize].blocked,
+            );
+        }
+    }
 
+    let mut previous_player_position = (-1, -1);
 
     while !tcod.root.window_closed() {
         tcod.con.clear();
 
-        render_all(&mut tcod, &game, &game_objects);
+        let fov_need_recompute = previous_player_position != (game_objects[0].x, game_objects[0].y);
+        render_all(&mut tcod, &game, &game_objects, fov_need_recompute);
 
         tcod.root.flush();
 
         let player = &mut game_objects[0];
+        previous_player_position = (player.x, player.y);
         let exit = handle_keys(&mut tcod, &game, player);
         if exit {
             break;
